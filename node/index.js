0f1156b3d4b03a83ef22bddac0838bd47a41b9bb
root <root@CamilaMax-PC>
2023-07-28 15:58:44 -0300

    - Acesso ao banco através da aplicação node

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'mysql',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Max')`
+connection.query(sql)
+connection.end()
 
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle (Response22)')
